edcd45e5a6333971a0f90bc0ea0e6ea36ff182f1
JoseBenin82 <julio_chivas2898@hotmail.com>
2026-02-12 17:42:01 -0600

    "Proyecto_Objetos_javascript"

--- clase_5/Proyecto_Objetos_en _JavaScript.js
+++ clase_5/Proyecto_Objetos_en _JavaScript.js
@@ -4,15 +4,36 @@ const libro = {
     autor: "Miguel de Cervantes",
     anio: 1605,
     estado: "disponible",
+    capitulos: [],
 
-    info (){
+    info() {
         console.log(`Título: ${this.titulo}`);
         console.log(`Autor: ${this.autor}`);
         console.log(`Año de publicación: ${this.anio}`);
         console.log(`Estado: ${this.estado}`);
+    },
+
+    agregarCapitulo(nombre) {
+        this.capitulos.push(nombre);
+        console.log(`Capítulo "${nombre}" agregado.`);
+    },
+
+    eliminarCapitulo(nombre) {
+        const index = this.capitulos.indexOf(nombre);
+        if (index !== -1) {
+            this.capitulos.splice(index, 1);
+            console.log(`Capítulo "${nombre}" eliminado.`);
+        }
+        else { console.log(`El capítulo "${nombre}" no existe.`); }
     }
 
 }
+
+libro.info();
+libro.agregarCapitulo("Capítulo I");
+libro.agregarCapitulo("Capítulo II");
+
+//libro.info(); libro.eliminarCapitulo("Capítulo I");
 
 libro.info();
 

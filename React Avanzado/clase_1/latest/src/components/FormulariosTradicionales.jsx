37b46762d05dfe2c4e49868e3205894f820bf314
JoseBenin82 <julio_chivas2898@hotmail.com>
2026-07-02 15:18:52 -0600

    add React Avanzado project

---
+++ React Avanzado/clase_1/latest/src/components/FormulariosTradicionales.jsx
@@ -0,0 +1,11 @@
+funcion FormularioTradicional(){
+    return (
+
+        <h2>Formulario tradicional<h2>
+            <form>
+            Name<input></input>
+            </form>
+    )
+}
+
+export defautl FormularioTradicional
